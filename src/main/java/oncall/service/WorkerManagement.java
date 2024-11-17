c295f9a90431b68a4ba7b881e659f9447834cf36
menzzi <minmin3927@naver.com>
2024-11-17 16:15:08 +0900

    feat(service) : WorkerManagement - convertStringToDeque 구현

--- src/main/java/oncall/service/WorkerManagement.java
+++ src/main/java/oncall/service/WorkerManagement.java
@@ -4,10 +4,21 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import oncall.validation.Validation;
 
 public class WorkerManagement {
-    public Deque<String> convertStringToQueue(String input){
+    public Deque<String> convertStringToDeque(String input){
         Deque<String> workers = new LinkedList<>();
+        Validation.validateInputFormat(input);
+
         List<String> nickNames = Arrays.stream(input.split(",")).toList();
+        for(String nickname : nickNames){
+            Validation.validateNickName(nickname);
+            if(!workers.contains(nickname)){
+                workers.add(nickname);
+            }
+        }
+        Validation.validateWorkerManagement(nickNames.size(),workers.size());
+        return workers;
     }
 }
