fcd80e7663bd4d41c8f32b17fa9cd1b30c426ed2
menzzi <minmin3927@naver.com>
2024-11-23 17:50:23 +0900

    feat(view) : 코치 이름 중복 확인 추가

--- src/main/java/menu/controller/RecommendationController.java
+++ src/main/java/menu/controller/RecommendationController.java
@@ -2,7 +2,6 @@ package menu.controller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import menu.model.Menu;
 import menu.model.RecommendedCategories;
@@ -14,7 +13,7 @@ public class RecommendationController {
     private final InputView input;
     private final OutputView output;
 
-    private final List<String> dayOfTheWeek = new ArrayList<>(Arrays.asList(("월요일,화요일,수요일,목요일,금요일").split(",")));
+    private final List<String> dayOfTheWeek = new ArrayList<>(Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일"));
     private final List<RecommendedMenus> recommendedMenus = new ArrayList<>();
 
     public RecommendationController(InputView input, OutputView output) {
@@ -27,21 +26,26 @@ public class RecommendationController {
         List<String> coachNames = inputCoachNames();
         RecommendedCategories recommendedCategories = recommendCategory();
         recommendMenu(recommendedCategories.getRecommendedCategories(),coachNames);
-        printResult();
+        printResult(recommendedCategories);
     }
 
     private RecommendedCategories recommendCategory(){
         RecommendedCategories recommendedCategories = new RecommendedCategories(new ArrayList<>());
-        int index = 0;
-        while(index < dayOfTheWeek.size()){
-            String category = Menu.recommendCategory();
-            try{
-                recommendedCategories.updateCategories(category);
-                index ++;
-            }catch (IllegalArgumentException ignored){
-            }
+        for (String day : dayOfTheWeek) {
+            repeatRecommendCategory(recommendedCategories);
         }
         return recommendedCategories;
+    }
+
+    private void repeatRecommendCategory(RecommendedCategories recommendedCategories){
+        while (true) {
+            String category = Menu.recommendCategory();
+            try {
+                recommendedCategories.updateCategories(category);
+                break;
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
     }
 
     private void recommendMenu(List<String> recommendedCategories, List<String> coachNames){
@@ -55,11 +59,13 @@ public class RecommendationController {
         }
     }
 
-    private void updateMenu(RecommendedMenus recommendedMenu, Menu menu){
-        try{
-            recommendedMenu.updateMenu(menu.recommendMenu());
-        }catch (IllegalArgumentException e){
-            updateMenu(recommendedMenu, menu);
+    private void updateMenu(RecommendedMenus eachCoachMenus, Menu menu){
+        while (true) {
+            try {
+                eachCoachMenus.updateMenu(menu.recommendMenu());
+                break;
+            } catch (IllegalArgumentException ignored) {
+            }
         }
     }
 
@@ -79,16 +85,18 @@ public class RecommendationController {
     }
 
     private List<String> inputCoachNames(){
-        try{
-            return input.inputCoachNames();
-        }catch (IllegalArgumentException e){
-            output.printErrorMessage(e.getMessage());
-            return inputCoachNames();
+        while (true) {
+            try {
+                return input.inputCoachNames();
+            } catch (IllegalArgumentException e) {
+                output.printErrorMessage(e.getMessage());
+            }
         }
     }
 
-    private void printResult(){
+    private void printResult(RecommendedCategories recommendedCategories){
         output.printResult(dayOfTheWeek);
+        output.printMenuRecommendationResult(recommendedCategories.toString());
         for(RecommendedMenus recommendedMenu: recommendedMenus){
             output.printMenuRecommendationResult(recommendedMenu.toString());
         }
